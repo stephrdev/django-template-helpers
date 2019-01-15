b42997003bfe467868c718d9afceec41a3f8aea5
Magdalena Rother <magdalena.rother@moccu.com>
2019-01-15 09:06:04 +0100

    Improve tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -3,10 +3,30 @@ from template_helpers.views import GenericTemplateView
 
 class TestGenericTemplateView:
 
-    def test_get_template_names(self, client):
-        view = GenericTemplateView(kwargs={'template': 'foo'})
-        assert view.get_template_names() == ['tests/foo.html']
+    def test_get_template_names(self):
+        view = GenericTemplateView()
+        view.kwargs = {}
 
-    def test_get_template_names_without_template(self, client):
-        view = GenericTemplateView(kwargs={'template': None})
-        assert view.get_template_names() == ['tests/index.html']
+        view.kwargs['base_dir'] = None
+        view.kwargs['template'] = None
+        assert view.get_template_names() == ['index.html']
+
+        view.kwargs['base_dir'] = None
+        view.kwargs['template'] = 'test123'
+        assert view.get_template_names() == ['test123.html']
+
+        view.kwargs['base_dir'] = None
+        view.kwargs['template'] = 'abc654/test123'
+        assert view.get_template_names() == ['abc654/test123.html']
+
+        view.kwargs['base_dir'] = 'foo'
+        view.kwargs['template'] = None
+        assert view.get_template_names() == ['foo/index.html']
+
+        view.kwargs['base_dir'] = 'foo'
+        view.kwargs['template'] = 'test123'
+        assert view.get_template_names() == ['foo/test123.html']
+
+        view.kwargs['base_dir'] = 'foo/bar/bazz'
+        view.kwargs['template'] = 'abc654/test123'
+        assert view.get_template_names() == ['foo/bar/bazz/abc654/test123.html']
