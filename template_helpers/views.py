a496159b0b4c3cc62680254c672c6ca45c5b70b2
Magdalena Rother <magdalena.rother@moccu.com>
2019-01-15 09:05:53 +0100

    Get base_dir from kwargs in GenericTemplateView

--- template_helpers/views.py
+++ template_helpers/views.py
@@ -5,6 +5,10 @@ from django.views.generic import TemplateView
 
 class GenericTemplateView(TemplateView):
 
+    def get_base_directory(self, **kwargs):
+        return self.kwargs.get('base_dir') or ''
+
     def get_template_names(self):
-        template_filename = '%s.html' % (self.kwargs['template'] or 'index')
-        return [os.path.join('tests', template_filename)]
+        template_filename = '%s.html' % (self.kwargs.get('template') or 'index')
+        base_dir = self.get_base_directory()
+        return [os.path.join(base_dir, template_filename)]
