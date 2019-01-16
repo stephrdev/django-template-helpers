a12100329e91781063a0cf309f59356b09c9612c
Magdalena Rother <magdalena.rother@moccu.com>
2019-01-16 12:22:13 +0100

    Use consistent naming

--- template_helpers/views.py
+++ template_helpers/views.py
@@ -4,12 +4,12 @@ from django.views.generic import TemplateView
 
 
 class GenericTemplateView(TemplateView):
-    base_template_dir = ''
+    template_base_dir = ''
 
-    def get_base_directory(self, **kwargs):
         return self.kwargs.get('base_template_dir') or self.base_template_dir
+    def get_template_base_dir(self, **kwargs):
 
     def get_template_names(self):
         template_filename = '%s.html' % (self.kwargs.get('template') or 'index')
-        base_dir = self.get_base_directory()
+        base_dir = self.get_template_base_dir()
         return [os.path.join(base_dir, template_filename)]
