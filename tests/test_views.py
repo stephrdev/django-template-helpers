86fbd7f44e5cac646e843adf2ca08381bc84f49c
Magdalena Rother <magdalena.rother@moccu.com>
2019-01-16 12:23:05 +0100

    Update tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -16,14 +16,20 @@ class TestGenericTemplateView:
         view.kwargs['template'] = 'abc654/test123'
         assert view.get_template_names() == ['abc654/test123.html']
 
-        view.kwargs['base_template_dir'] = 'foo'
-        view.kwargs['template'] = None
-        assert view.get_template_names() == ['foo/index.html']
+    def test_get_template_base_dir(self, rf):
+        view = GenericTemplateView.as_view(template_base_dir='test_dir')
+        request = rf.get('/')
 
-        view.kwargs['base_template_dir'] = 'foo'
-        view.kwargs['template'] = 'test123'
-        assert view.get_template_names() == ['foo/test123.html']
+        response = view(request)
+        assert response.template_name[0] == 'test_dir/index.html'
 
-        view.kwargs['base_template_dir'] = 'foo/bar/bazz'
-        view.kwargs['template'] = 'abc654/test123'
-        assert view.get_template_names() == ['foo/bar/bazz/abc654/test123.html']
+        response = view(request, **{'template': 'test123'})
+        assert response.template_name[0] == 'test_dir/test123.html'
+
+        view = GenericTemplateView.as_view(template_base_dir='test_dir/foo/bar')
+
+        response = view(request)
+        assert response.template_name[0] == 'test_dir/foo/bar/index.html'
+
+        response = view(request, **{'template': 'abc654/test123'})
+        assert response.template_name[0] == 'test_dir/foo/bar/abc654/test123.html'
